e18cbd909c161d6ecc6e3c1c35596681bd4ddfc7
Rail Mazgutov <rail.mazgutov@gmail.com>
2017-12-12 15:53:14 +0300

    Add some methoda implementations

--- Asio-Server-and-Client/Server/BaseServer.cpp
+++ Asio-Server-and-Client/Server/BaseServer.cpp
@@ -14,7 +14,6 @@
 #include <asio/ip/tcp.hpp>
 
 const std::string BaseServer::DEFAULT_LOCAL_IP	= "127.0.0.1";
-const unsigned int BaseServer::DEFAULT_PORT		= 8080;
 
 namespace
 {
@@ -74,25 +73,83 @@ namespace
 	};
 	 
 
-	template<typename MessageReceived, typename ClientConnected = void, typename ClientDisconnected = void>
-	class TcpServer final : public BaseServer
+	class TcpServer final : public BaseServer, public std::enable_shared_from_this<TcpServer>
 	{
-		using Server = TcpServer<MessageReceived, ClientConnected, ClientDisconnected>;
-		using Client_t = Client<Server, 1024 * 1024>;
+	public:
+		using Client_t = Client<TcpServer, 1024 * 1024>;
 		friend Client_t;
-		TcpServer();
+		TcpServer(): address(ip::address::from_string("127.0.0.1")),
+		             endPoint(address, 8080),
+		             acceptor(service, endPoint),
+		             socket(service), lastClientId(0)
+		{
+		}
+
+		TcpServer(std::function<void(std::shared_ptr<Message>)> callback) : messageReceivedCallback(callback),
+			address(ip::address::from_string("127.0.0.1")),
+			endPoint(address, 8080),
+			acceptor(service, endPoint), socket(service),
+			lastClientId(0)
+		{
+		}
+
 		void run() override
 		{
-			std::cout << "TcpServer:run" << std::endl;
+			if (!running) {
+				running = true;
+				std::unique_lock<std::mutex> section(messages_lock);
+				for (int i = 0; i < MAX_THREADS_COUNT; i++)
+				{
+					messageHandlers.emplace_back(std::make_shared<std::thread>(
+						[&running = running, &messages = messages, &lock = messages_lock] (auto& callback) mutable
+						{
+							while (running) {
+								std::shared_ptr<Message> message;
+								{
+									std::unique_lock<std::mutex> section(lock);
+									if (!running)
+										break;
+									if (messages.size() > 0) {
+										message = messages.front();
+										messages.pop();
+									}
+								}
+								if (message)
+								{
+									if(callback)
+									{
+										callback(message);
+									}
+								}
+							}
+						}, messageReceivedCallback)
+					);
+				}
+				service.run();
+			}
 		}
 		void stop() override
 		{
-			std::cout << "TcpServer:stop" << std::endl;
+			if(running)
+			{
+				running = false;
+				for (auto client : clients)
+					client.second->close();
+
+				for (auto thread : messageHandlers) {
+					thread->join();
+				}
+				messageHandlers.clear();
+				acceptor.close();
+				service.stop();
+			}
+			
 		}
 
 		void sendMessage(Message& message) override
 		{
-			std::cout << "TcpServer:sendMessage" << std::endl;
+			if (clients.find(message.sender) != clients.end())
+				clients[message.sender]->sendMessgae(message.message);
 		}
 		void sendMessageToAll(std::string& message) override
 		{
@@ -100,29 +157,63 @@ namespace
 		}
 		size_t connectionsCount() override
 		{
-			std::cout << "TcpServer:connectionsCount" << std::endl;
-			return 0;
+			return clients.size();
 		}
 		
+		~TcpServer()
+		{
+			std::for_each(messageHandlers.begin(), messageHandlers.end(), [](auto handler)mutable
+			{
+				handler->detach();
+			});
+			messageHandlers.clear();
+		}
 	private:
-		
+		void acceptClient()
+		{
+			acceptor.async_accept(socket,
+				[this](std::error_code ec)
+			{
+				if (!ec)
+				{
+					auto client = std::make_shared<Client_t>(lastClientId, std::move(socket), shared_from_this());
+					client->run();
+					{
+						std::unique_lock<std::mutex> critical_section(clients_lock);
+						clients.insert(std::make_pair(lastClientId, client));
+						lastClientId++;
+					}
+					if(clientConnectedCallback)
+					{
+						clientConnectedCallback(lastClientId - 1);
+					}
+				}
+				acceptClient();
+			});
+		}
 
 		void messageReceived(std::shared_ptr<Message> message)
 		{
-
+			std::unique_lock<std::mutex> lock(messages_lock);
+			messages.push(message);
 		}
 
 		void clientDisconnected(std::size_t id)
 		{
+			if (clientDisconnectedCallback)
+				clientDisconnectedCallback(id);
 
+			std::unique_lock<std::mutex> lock(clients_lock);
+			if (clients.find(id) != clients.end())
+				clients.erase(id);			
 		}
 
 		std::atomic<bool> running;
 
 		//Callbacks for events
-		MessageReceived messageReceivedCallback;
-		ClientConnected clientConnectedCallback;
-		ClientDisconnected clientDisconnectedCallback;
+		std::function<void(std::shared_ptr<Message>)> messageReceivedCallback;
+		std::function<void(std::size_t)> clientConnectedCallback;
+		std::function<void(std::size_t)> clientDisconnectedCallback;
 
 		//Asio for tcp
 		io_service service;
@@ -133,7 +224,9 @@ namespace
 
 		size_t lastClientId;
 
-		std::mutex lock;
+		std::mutex messages_lock;
+		std::mutex clients_lock;
+
 		std::queue<std::shared_ptr<Message>> messages;
 		std::vector<std::shared_ptr<std::thread>> messageHandlers;
 		std::map<size_t, std::shared_ptr<Client_t>> clients;
@@ -142,7 +235,7 @@ namespace
 
 //template<typename MessageReceived, typename ClientConnected, typename ClientDisconnected>
 //std::shared_ptr<BaseServer> getServer(ProtocolType type, MessageReceived msgHandler, ClientConnected connectionHandler, ClientDisconnected diconnectionHandler)
-std::shared_ptr<BaseServer> getServer(ProtocolType type, int msgHandler, int connectionHandler, int diconnectionHandler)
+std::shared_ptr<BaseServer> getServer(ProtocolType type, std::function<void(std::shared_ptr<Message>)> callback)
 {
-	return std::make_shared<TcpServer<int, int, int>>();
+	return std::make_shared<TcpServer>(callback);
 }
--- Asio-Server-and-Client/Server/BaseServer.h
+++ Asio-Server-and-Client/Server/BaseServer.h
@@ -11,6 +11,11 @@
 
 struct Message
 {
+	Message() = default;
+	Message(size_t id, std::string msg): sender(id), message(std::move(msg)) {
+		
+	}
+
 	size_t sender;
 	std::string message;
 };
@@ -21,11 +26,12 @@ enum class ProtocolType
 	UDP
 };
 
-class BaseServer
+SERVER_API class BaseServer
 {
 public:
+	static constexpr unsigned int MAX_THREADS_COUNT = 4;
 	static const std::string DEFAULT_LOCAL_IP;	// 127.0.0.1
-	static const unsigned int DEFAULT_PORT;		// 8080
+	static constexpr unsigned int DEFAULT_PORT = 8080;		// 8080
 
 	SERVER_API virtual ~BaseServer() = default;
 
@@ -42,4 +48,4 @@ public:
 };
 
 //template<typename MessageReceived, typename ClientConnected, typename ClientDisconnected>
-SERVER_API std::shared_ptr<BaseServer> getServer(ProtocolType type, int msgHandler, int connectionHandler, int diconnectionHandler);
+SERVER_API std::shared_ptr<BaseServer> getServer(ProtocolType type, std::function<void(std::shared_ptr<Message>)> callback);
